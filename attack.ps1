cc9a264a7cfec0954172c80db2981a7141222e4a
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-06-21 23:08:25 +0200

    Update attack.ps1

--- attack.ps1
+++ attack.ps1
@@ -1,3 +1,2 @@
 Install-Module AZ -Force
-Import-Module AZ
 New-AzRoleAssignment -SignInName 'h.sanders@securehats.nl' -Scope '/subscriptions/7570c6f7-9ca9-409b-aeaf-cb0f5ac1ad50' -RoleDefinitionName 'Owner'
