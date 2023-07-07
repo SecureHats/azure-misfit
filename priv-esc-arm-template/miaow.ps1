9d83bd01dae04dc6e3630df4549ff2da7dec8a6f
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-07-07 18:34:02 +0200

    Update miaow.ps1

--- priv-esc-arm-template/miaow.ps1
+++ priv-esc-arm-template/miaow.ps1
@@ -13,6 +13,25 @@ param (
     [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635'
 )
 
+$logo = "
+MMMMMMMM               MMMMMMMM  iiii                                                                           
+M:::::::M             M:::::::M i::::i                                                                          
+M::::::::M           M::::::::M  iiii                                                                           
+M:::::::::M         M:::::::::M                                                                                 
+M::::::::::M       M::::::::::Miiiiiii   aaaaaaaaaaaaa     ooooooooooo wwwwwww           wwwww           wwwwwww
+M:::::::::::M     M:::::::::::Mi:::::i   a::::::::::::a  oo:::::::::::oow:::::w         w:::::w         w:::::w 
+M:::::::M::::M   M::::M:::::::M i::::i   aaaaaaaaa:::::ao:::::::::::::::ow:::::w       w:::::::w       w:::::w  
+M::::::M M::::M M::::M M::::::M i::::i            a::::ao:::::ooooo:::::o w:::::w     w:::::::::w     w:::::w   
+M::::::M  M::::M::::M  M::::::M i::::i     aaaaaaa:::::ao::::o     o::::o  w:::::w   w:::::w:::::w   w:::::w    
+M::::::M   M:::::::M   M::::::M i::::i   aa::::::::::::ao::::o     o::::o   w:::::w w:::::w w:::::w w:::::w     
+M::::::M    M:::::M    M::::::M i::::i  a::::aaaa::::::ao::::o     o::::o    w:::::w:::::w   w:::::w:::::w      
+M::::::M     MMMMM     M::::::M i::::i a::::a    a:::::ao::::o     o::::o     w:::::::::w     w:::::::::w       
+M::::::M               M::::::Mi::::::ia::::a    a:::::ao:::::ooooo:::::o      w:::::::w       w:::::::w        
+M::::::M               M::::::Mi::::::ia:::::aaaa::::::ao:::::::::::::::o       w:::::w         w:::::w         
+M::::::M               M::::::Mi::::::i a::::::::::aa:::aoo:::::::::::oo         w:::w           w:::w          
+MMMMMMMM               MMMMMMMMiiiiiiii  aaaaaaaaaa  aaaa  ooooooooooo            www             www           
+                                                                                                                "
+                                                                                                                
 $token = (Get-AzAccessToken).token
 $headers = @{"Authorization"="Bearer $token"}
 $guid = (New-Guid).Guid
@@ -33,3 +52,5 @@ $result = Invoke-RestMethod `
     -ContentType 'application/json' `
     -Method 'PUT' `
     -headers $headers
+
+Write-Host $logo -ForegroundColor Blue
